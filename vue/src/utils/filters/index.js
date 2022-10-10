af33d144ad62f8740e9bfba58fc47daa59cbc238
Roman Bondarev <rbondarev@rdv-it.ru>
2022-10-10 22:10:37 +0300

    remove vue2

--- vue/src/utils/filters/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-Vue.filter('toByte', (value) => {
-    const sizes = ['Bytes', 'KB', 'MB'];
-    if (value == 0) return '0 Byte';
-    const i = parseInt(Math.floor(Math.log(value) / Math.log(1024)));
-    return Math.round(value / Math.pow(1024, i), 2) + ' ' + sizes[i];
-})
