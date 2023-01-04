3b7c5598747f5cc883ca9f0557b5fc039439f983
Roman Bondarev <rbondarev@rdv-it.ru>
2023-01-04 20:48:59 +0300

    init

--- src/const/struct.h
+++ src/const/struct.h
@@ -7,6 +7,7 @@ enum keys {
   PROGRESS,
   FILES,
   REBOOT,
+  GPIO,
   END
 };
 
@@ -69,4 +70,11 @@ struct Files {
   uint8_t command;
   uint32_t size;
   char name[32];
+};
+
+struct Gpio {
+  uint8_t key;
+  uint8_t gpio;
+  uint8_t presses;
+  uint8_t pressed;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,39 @@
 
 uint32_t now;
 
+Gpio btn = {GPIO, D1, 0, false};
+
+unsigned long button_time = 0;
+unsigned long last_button_time = 0;
+
+void IRAM_ATTR isr() {
+  button_time = millis();
+  if (button_time - last_button_time > 250) {
+    btn.presses++;
+    btn.pressed = true;
+    last_button_time = button_time;
+  }
+}
+
 void setup() {
   setupInit();
   setupServer(onWsEvent);
+  pinMode(btn.gpio, INPUT_PULLUP);
+  attachInterrupt(btn.gpio, isr, FALLING);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  digitalWrite(D5, HIGH);
+  digitalWrite(D6, HIGH);
 }
 
 void loop() {
   now = millis();
   loopServer(now);
   loopTask(now);
+  if (btn.pressed) {
+    btn.pressed = false;
+    digitalWrite(D5, !digitalRead(D5));
+    digitalWrite(D6, !digitalRead(D6));
+    wsSend((uint8_t *)&btn, sizeof(btn));
+  }
 }
