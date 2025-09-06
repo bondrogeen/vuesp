4d095828475d724f9852fe75102b27af6e22af31
Bondarev Roman <bondrogeen@gmail.com>
2025-09-06 16:21:52 +0300

    build

--- vue/src/components/app/AppAside.vue
+++ vue/src/components/app/AppAside.vue
@@ -22,7 +22,7 @@
 
           <ul class="mb-6 flex flex-col gap-4">
             <li v-for="{ name, path, icon } of menu" :key="name">
-              <router-link :to="path" class="flex gap-2 items-center px-3 py-2 rounded-sm" :class="isActive(path) ? 'bg-blue-500/10' : 'hover:bg-gray-500/10'" @click.prevent="onSelect(name)">
+              <router-link :to="path" class="flex gap-2 items-center px-3 py-2 rounded-sm" :class="isActive(path) ? 'bg-blue-500/10' : 'hover:bg-gray-500/10'" @click.prevent="onSelect()">
                 <component :is="icon" class="h-5 w-5 flex-[0_0_24px]" />
 
                 <span class="flex-1 first-letter:uppercase" :class="sidebarToggle ? 'lg:hidden' : ''">{{ name }}</span>
