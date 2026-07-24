026c9957aff65074a19e9f6fed5ea14fc02e6e92
Rooman Bondarev <bondrogeen@gmail.com>
2026-07-24 23:52:20 +0300

    feat: add example

--- README.md
+++ README.md
@@ -6,8 +6,15 @@ Template for your arduino projects with a web interface.
 
 [ESPAsyncWebServer](https://github.com/me-no-dev/ESPAsyncWebServer) - Async HTTP and WebSocket Server for ESP Arduino  
 [Vue3](https://vuejs.org/) - The Progressive JavaScript Framework
+[Tailwindcss](https://tailwindcss.com/) - css
+[Vue components](https://github.com/bondrogeen/vuesp-components) - vuesp-components
 
-![Vuesp Screenshots](./github/images/screen/home.webp)
+![Vuesp home](./github/images/screen/home.png)
+![Vuesp dashboard](./github/images/screen/dashboard.png)
+![Vuesp script](./github/images/screen/script.png)
+![Vuesp settings](./github/images/screen/settings.png)
+![Vuesp storage](./github/images/screen/storage.png)
+![Vuesp gpio](./github/images/screen/gpio.png)
 
 ## Features
 
@@ -160,7 +167,6 @@ For production deployment, please read [SECURITY.md](./SECURITY.md) for:
 - **HTTP Server** - ESPAsyncWebServer
 - **WebSocket** - Binary protocol for efficiency
 - **File System** - LittleFS support
-- **Tasks** - FreeRTOS task scheduling
 - **Configuration** - EEPROM storage
 
 ### Communication Protocol
@@ -172,6 +178,11 @@ Binary WebSocket messages at `/esp` endpoint:
 - All platform-specific
 
 ## Version History
+
+### 3.4.0 (2026-07-24)
+
+- Update Web
+- Add example components
 
 ### 3.2.0 (2025-07-03)
 
--- vue/src/pages/index.vue
+++ vue/src/pages/index.vue
@@ -1,38 +1,210 @@
 <template>
-  <div class="container mx-auto">
-    <div class="mb-6 flex items-center justify-between">
+  <div class="container mx-auto pb-10">
+    <section class="mb-6 flex items-center justify-between">
       <h1>{{ $t('home') }}</h1>
 
-      <v-select :items="listMenu" @change="onMenu">
+      <v-select modelValue="2" :items="listMenu" @change="onMenu">
         <template #activator="{ on }">
-          <v-button color="" type="icon" @click="on.click">
+          <v-button type="icon" @click="on.click">
             <icon-ri-more-line class="rotate-90"></icon-ri-more-line>
           </v-button>
         </template>
       </v-select>
-    </div>
+    </section>
 
-    <div class="flex flex-col gap-6">
+    <div>
       <p class="text-lg font-bold mb-4 text-gray-400 max-w-200">
         {{ pkg.description }}
       </p>
+    </div>
 
-      <card-main :title="$t('icons')">
-        <div class="flex gap-3 mb-6">
-          <icon-ri-home-line />
-          <icon-ri-mail-line class="hover:scale-130 transition-all" />
-          <icon-ri-printer-line class="hover:text-blue-500 transition-all" />
-          <icon-ri-chat-1-line class="text-blue-500" />
-          <icon-ri-settings-2-line class="size-4" />
+    <section class="flex flex-col gap-6">
+      <div>
+        <div class="flex items-center gap-6 mb-5">
+          <h2 class="">Css</h2>
+          <a href="https://tailwindcss.com/" target="_blank" class="text-lg text-blue-400 underline">https://tailwindcss.com/</a>
         </div>
 
-        <v-button color="blue" href="https://remixicon.com/" target="_blank">https://remixicon.com/</v-button>
-      </card-main>
-
-      <div>
-        {{ main.info }}
+        <div class="flex items-center justify-center rounded h-5 bg-green-500 md:bg-red-500 lg:bg-blue-500 xl:bg-gray-300 2xl:bg-yellow-500 uppercase py-4">
+          <span class="text-white sm:hidden">sm</span>
+          <span class="hidden sm:block md:hidden text-white">sm</span>
+          <span class="hidden md:block lg:hidden text-white">md</span>
+          <span class="hidden lg:block xl:hidden text-white">lg</span>
+          <span class="hidden xl:block xl2:hidden text-black">xl</span>
+          <span class="hidden xl2:block text-white">xl2</span>
+        </div>
       </div>
-    </div>
+
+      <div class="">
+        <div class="flex items-center gap-6 mb-5">
+          <h2>Icon</h2>
+          <a href="https://remixicon.com/" target="_blank" class="text-lg text-blue-400 underline">https://remixicon.com/</a>
+        </div>
+
+        <div>
+          <div class="flex items-center gap-3 mb-6">
+            <icon-ri-home-line />
+            <icon-ri-mail-line class="hover:scale-130 transition-all" />
+            <icon-ri-printer-line class="hover:text-blue-500 transition-all" />
+            <icon-ri-chat-1-line class="text-blue-500" />
+            <icon-ri-settings-2-line class="size-4" />
+            <v-button color="blue" href="https://remixicon.com/" target="_blank">https://remixicon.com/</v-button>
+          </div>
+        </div>
+      </div>
+
+      <div class="">
+        <div>
+          <div class="flex items-center gap-6 mb-5">
+            <h2>Components</h2>
+            <a href="https://github.com/bondrogeen/vuesp-components" target="_blank" class="text-lg text-blue-400 underline">vuesp-components</a>
+          </div>
+
+          <h3 class="mb-5">Button</h3>
+
+          <div class="flex flex-wrap gap-3 mb-6">
+            <v-button color="blue" size="sm" @click="dialog = true">Dialog</v-button>
+            <v-button color="red" size="md" @click="dialog = true">Dialog</v-button>
+            <v-button color="green" size="lg" :loading="loading" @click="onLoading">Loading</v-button>
+            <v-button color="gray" size="xl" :loading="loading" @click="onLoading">Loading</v-button>
+          </div>
+
+          <div class="flex flex-wrap gap-3 mb-6">
+            <v-button outline color="blue" size="sm" @click="dialog = true">Dialog</v-button>
+            <v-button outline color="red" size="md" @click="dialog = true">Dialog</v-button>
+            <v-button outline color="green" size="lg" :loading="loading" @click="onLoading">Loading</v-button>
+            <v-button outline color="gray" size="xl" :loading="loading" @click="onLoading">Loading</v-button>
+            <v-button outline color="transparent" size="xl" class="text-white bg-yellow-400 dark:bg-yellow-800" :loading="loading" @click="onLoading">Custom color</v-button>
+          </div>
+
+          <div class="flex flex-wrap gap-3 mb-6">
+            <v-button icon color="blue" size="sm" @click="dialog = true">
+              <icon-ri-home-line />
+            </v-button>
+            <v-button outline color="red" size="md" @click="dialog = true">
+              <icon-ri-mail-line class="hover:scale-130 transition-all" />
+            </v-button>
+            <v-button outline color="green" size="lg" loading @click="onLoading">
+              <icon-ri-printer-line class="hover:text-blue-500 transition-all" />
+            </v-button>
+            <v-button outline color="gray" size="xl" :loading="loading" @click="onLoading">
+              <icon-ri-home-line />
+            </v-button>
+          </div>
+        </div>
+
+        <div>
+          <h3 class="mb-5">Form</h3>
+
+          <div class="flex flex-col gap-3">
+            <VTextField v-model="input" label="Text" />
+
+            <VTextField label="Error" message="message">
+              <template #icon>
+                <v-button color="transparent" @click="dialog = true">
+                  <icon-ri-home-line />
+                </v-button>
+              </template>
+            </VTextField>
+
+            <VSelect :model-value="getSelectValue" :items="items" label="Select" @change="onSelect" />
+
+            <div class="flex gap-3 mt-5">
+              <VCheckbox v-model="checkbox">Checkbox</VCheckbox>
+              <VCheckbox @update:model-value="checkbox = !checkbox">!Checkbox</VCheckbox>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="">
+        <h3 class="mb-5">Card</h3>
+
+        <card-main :title="$t('Form')" :loading="loading">
+          <div v-if="loading" class="absolute top-0 left-0 flex items-center justify-center h-full w-full bg-black/40 rounded">
+            <VLoader />
+          </div>
+
+          <div class="flex flex-col gap-3">
+            <div class="flex items-center gap-3">
+              <h3 class="self-start">main.ports:</h3>
+              <p>{{ main.ports }}</p>
+            </div>
+            <div class="flex items-center gap-3">
+              <h3 class="self-start">main.device:</h3>
+              <p>{{ main.device }}</p>
+            </div>
+            <div class="flex items-center gap-3">
+              <h3 class="self-start">main.info:</h3>
+              <p>{{ main.info }}</p>
+            </div>
+          </div>
+        </card-main>
+      </div>
+
+      <div class="">
+        <h3 class="mb-5">Table</h3>
+
+        <VTable :headers="tabletHeaders" :items="tabletItems" @click="onTable">
+          <template #name="{ item, i }">
+            <span class="me-2"># {{ i + 1 }}</span>
+            <span class="uppercase text-black dark:text-white">{{ item.name }}</span>
+          </template>
+
+          <template #action="{ item }">
+            <div>
+              <VDropdown :hide-on-click="false" right="0" left="unset" class="relative">
+                <template #activator="{ on }">
+                  <v-button v-if="item.action" color="transparent" size="sm" @click.stop="on.click">
+                    <icon-ri-more-line class="rotate-90"></icon-ri-more-line>
+                  </v-button>
+                </template>
+
+                <div class="bg-white dark:bg-gray-900 rounded w-50 shadow-nav p-4">
+                  <p class="">
+                    {{ item }}
+                  </p>
+                </div>
+              </VDropdown>
+            </div>
+          </template>
+        </VTable>
+      </div>
+
+      <div class="">
+        <h3 class="mb-5">Expansion</h3>
+
+        <VExpansion label="Item 1" value>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, illum repudiandae debitis laboriosam voluptatum amet nemo optio est! Recusandae sapiente quos tempore nostrum sunt repudiandae
+          cupiditate consequuntur perferendis temporibus dolorem?
+        </VExpansion>
+
+        <VExpansion label="Item 1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, illum repudiandae debitis laboriosam voluptatum amet nemo optio est! Recusandae sapiente quos tempore nostrum sunt repudiandae
+          cupiditate consequuntur perferendis temporibus dolorem?
+        </VExpansion>
+
+        <VExpansion label="Item 1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, illum repudiandae debitis laboriosam voluptatum amet nemo optio est! Recusandae sapiente quos tempore nostrum sunt repudiandae
+          cupiditate consequuntur perferendis temporibus dolorem?
+        </VExpansion>
+      </div>
+    </section>
+
+    <v-dialog v-if="dialog" size="md" title="List" @close="dialog = false">
+      <VList :items="tabletItems">
+        <template #default="{ item }">{{ item.name }} ({{ item.date }})</template>
+      </VList>
+    </v-dialog>
+
+    <v-dialog v-if="dialogTable" size="md" title="Dialog table" @close="dialogTable = false">
+      <h4 class="mb-5">
+        {{ tabletItem?.name }}
+      </h4>
+      <p>
+        {{ tabletItem }}
+      </p>
+    </v-dialog>
   </div>
 </template>
 
@@ -42,14 +214,70 @@ import type { IListItem } from '@/types';
 import { KEYS } from '@/types';
 
 import { useConnection } from '@/composables/useConnection';
-
 import { useLocale } from '@/composables/useLocale';
+
+import { VButton, VLoader, VCheckbox, VDropdown, VExpansion, VList, VSelect, VTextField, VTable, VDialog } from 'vuesp-components';
+import { computed, ref } from 'vue';
+import { dateUtcToString, timeUtcToString } from 'vuesp-components/helpers';
 
 const { $t } = useLocale();
 
 const { pkg, main, onSend } = useConnection((send) => {
   send(KEYS.DEVICE);
 });
+
+interface IItemTable {
+  name: string;
+  date: string;
+  action: boolean;
+}
+
+const dialogTable = ref(false);
+const tabletItem = ref<IItemTable | null>(null);
+
+const tabletHeaders = [
+  { name: 'Name', key: 'name', className: 'w-50' },
+  { name: 'Date', key: 'date', className: '' },
+  { name: 'Action', key: 'action', className: 'w-20' },
+];
+const tabletItems: IItemTable[] = [
+  { name: 'Item 1', date: `${dateUtcToString(new Date())} ${timeUtcToString(new Date())}`, action: false },
+  { name: 'Item 2', date: `${dateUtcToString(new Date())} ${timeUtcToString(new Date())}`, action: true },
+  { name: 'Item 3', date: `${dateUtcToString(new Date())} ${timeUtcToString(new Date())}`, action: true },
+  { name: 'Item 4', date: `${dateUtcToString(new Date())} ${timeUtcToString(new Date())}`, action: false },
+];
+
+const onTable = (item: IItemTable) => {
+  tabletItem.value = item;
+  dialogTable.value = true;
+};
+
+const dialog = ref(false);
+
+const input = ref('');
+
+const checkbox = ref(true);
+
+const loading = ref(false);
+const onLoading = () => {
+  loading.value = true;
+  setTimeout(() => {
+    loading.value = false;
+  }, 1000);
+};
+
+const items: IListItem[] = [
+  { name: 'Item 1', value: 1 },
+  { name: 'Item 2', value: 2 },
+  { name: 'Item 3', value: '3' },
+];
+
+const select = ref<number | string>(2);
+const getSelectValue = computed(() => items.find((i) => i.value == select.value)?.name || '');
+const onSelect = ({ name, value }: IListItem) => {
+  select.value = value;
+  input.value = name;
+};
 
 const listMenu: IListItem[] = [{ name: $t('btnUpdate'), value: 2 }];
 
